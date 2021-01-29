a41de6bf66ae78140dc9d624510b66265f885fbb
shockerli <shockerli@upfor.club>
2021-01-29 11:25:00 +0800

    robot msg send functions

--- robot.go
+++ robot.go
@@ -10,27 +10,104 @@ import (
 	"time"
 )
 
-// 群机器人-自定义
+// RobotCustom 群机器人-自定义
 // @doc https://developers.dingtalk.com/document/app/custom-robot-access
 type RobotCustom struct {
-	apiUri      string
+	apiURI      string
 	accessToken string
 	secret      string
 }
 
+// NewRobotCustom 实例化
 func NewRobotCustom() *RobotCustom {
 	return &RobotCustom{
-		apiUri: "https://oapi.dingtalk.com/robot/send",
+		apiURI: "https://oapi.dingtalk.com/robot/send",
 	}
 }
 
+// SendText 发送Text消息
+func (r RobotCustom) SendText(content string, opts ...RobotOption) error {
+	msg := &robotMsg{
+		MsgType: msgTypeText,
+		Text:    robotText{Content: content},
+	}
+	for _, opt := range opts {
+		opt(msg)
+	}
+	return r.send(msg)
+}
+
+// SendLink 发送Link消息
+func (r RobotCustom) SendLink(title, text, msgURL, picURL string, opts ...RobotOption) error {
+	msg := &robotMsg{
+		MsgType: msgTypeLink,
+		Link: robotLink{
+			Title:      title,
+			Text:       text,
+			MessageURL: msgURL,
+			PicURL:     picURL,
+		},
+	}
+	for _, opt := range opts {
+		opt(msg)
+	}
+	return r.send(msg)
+}
+
+// SendMarkdown 发送Markdown消息
+func (r RobotCustom) SendMarkdown(title, text string, opts ...RobotOption) error {
+	msg := &robotMsg{
+		MsgType: msgTypeMarkdown,
+		Markdown: robotMarkdown{
+			Title: title,
+			Text:  text,
+		},
+	}
+	for _, opt := range opts {
+		opt(msg)
+	}
+	return r.send(msg)
+}
+
+// SendActionCard 发送ActionCard消息
+func (r RobotCustom) SendActionCard(title, text string, opts ...RobotOption) error {
+	msg := &robotMsg{
+		MsgType: msgTypeActionCard,
+		ActionCard: robotActionCard{
+			Title:          title,
+			Text:           text,
+			HideAvatar:     "0", // 默认展示
+			BtnOrientation: "1", // 默认横向排列
+		},
+	}
+	for _, opt := range opts {
+		opt(msg)
+	}
+	return r.send(msg)
+}
+
+// SendFeedCard 发送FeedCard消息
+func (r RobotCustom) SendFeedCard(opts ...RobotOption) error {
+	msg := &robotMsg{
+		MsgType: msgTypeFeedCard,
+		FeedCard: robotFeedCard{
+			Links: []robotFeedCardLink{},
+		},
+	}
+	for _, opt := range opts {
+		opt(msg)
+	}
+	return r.send(msg)
+}
+
+// 发送消息
 func (r RobotCustom) send(msg interface{}) error {
 	v, err := json.Marshal(msg)
 	if err != nil {
 		return err
 	}
 
-	var value url.Values
+	var value = make(url.Values)
 	value.Set("access_token", r.accessToken)
 	if r.secret != "" {
 		t := time.Now().UnixNano() / 1e6
@@ -38,7 +115,7 @@ func (r RobotCustom) send(msg interface{}) error {
 		value.Set("sign", r.sign(t, r.secret))
 	}
 
-	data, err := request(r.apiUri+"?"+value.Encode(), v)
+	data, err := request(r.apiURI+"?"+value.Encode(), v)
 	if err != nil {
 		return err
 	}
@@ -58,6 +135,7 @@ func (r RobotCustom) send(msg interface{}) error {
 	return nil
 }
 
+// 签名算法
 func (RobotCustom) sign(ts int64, secret string) string {
 	h := hmac.New(sha256.New, []byte(secret))
 	h.Write([]byte(fmt.Sprintf("%d\n%s", ts, secret)))
@@ -86,18 +164,19 @@ type robotMsg struct {
 	FeedCard   robotFeedCard   `json:"feedCard,omitempty"`
 }
 
-// 类型: Text
-type robotText struct {
-	Content string `json:"content"` // 消息内容
-}
-
-// @人
+// 消息@人的设置
+// [NOTICE] 仅针对Text/Link/Markdown类型有效
 type robotAt struct {
 	AtMobiles []string `json:"atMobiles,omitempty"` // 被@人的手机号
 	IsAtAll   bool     `json:"isAtAll,omitempty"`   // 是否@所有人
 }
 
-// 类型: Link
+// 消息类型: Text
+type robotText struct {
+	Content string `json:"content"` // 消息内容
+}
+
+// 消息类型: Link
 type robotLink struct {
 	Title      string `json:"title"`            // 消息标题
 	Text       string `json:"text"`             // 消息内容，如果太长只会部分展示
@@ -105,13 +184,13 @@ type robotLink struct {
 	PicURL     string `json:"picUrl,omitempty"` // 图片URL
 }
 
-// 类型: Markdown
+// 消息类型: Markdown
 type robotMarkdown struct {
 	Title string `json:"title"` // 首屏会话透出的展示内容
 	Text  string `json:"text"`  // Markdown格式的消息
 }
 
-// 类型: ActionCard
+// 消息类型: ActionCard
 // * 整体跳转
 // * 独立跳转
 // [NOTICE]设置singleTitle和singleURL后，btns无效
@@ -130,7 +209,7 @@ type robotActionCardBtn struct {
 	ActionURL string `json:"actionURL"` // 点击按钮触发的URL
 }
 
-// 类型: FeedCard
+// 消息类型: FeedCard
 type robotFeedCard struct {
 	Links []robotFeedCardLink `json:"links"`
 }
@@ -139,4 +218,71 @@ type robotFeedCardLink struct {
 	Title      string `json:"title"`
 	MessageURL string `json:"messageURL"` // 点击单条信息到跳转链接
 	PicURL     string `json:"picURL"`     // 单条信息后面图片的URL
+}
+
+// RobotOption 群机器人-消息配置项
+type RobotOption func(*robotMsg)
+
+// AtAll 设置是否@所有人
+// [NOTICE] 仅针对Text/Link/Markdown类型有效
+func (r RobotCustom) AtAll() RobotOption {
+	return func(msg *robotMsg) {
+		msg.At.IsAtAll = true
+	}
+}
+
+// AtMobiles 设置@人的手机号
+// [NOTICE] 仅针对Text/Link/Markdown类型有效
+func (r RobotCustom) AtMobiles(m ...string) RobotOption {
+	return func(msg *robotMsg) {
+		msg.At.AtMobiles = m
+	}
+}
+
+// HideAvatar 隐藏缩略图
+// 仅针对ActionCard类型
+func (r RobotCustom) HideAvatar(v string) RobotOption {
+	return func(msg *robotMsg) {
+		msg.ActionCard.HideAvatar = v
+	}
+}
+
+// BtnOrientation 按钮排列(0: 竖直排列, 1: 横向排列)
+// 仅针对ActionCard类型
+func (r RobotCustom) BtnOrientation(v string) RobotOption {
+	return func(msg *robotMsg) {
+		msg.ActionCard.BtnOrientation = v
+	}
+}
+
+// SingleCard 整体调整配置
+// 仅针对ActionCard类型
+func (r RobotCustom) SingleCard(title, url string) RobotOption {
+	return func(msg *robotMsg) {
+		msg.ActionCard.SingleTitle = title
+		msg.ActionCard.SingleURL = url
+	}
+}
+
+// MultiCard 添加一个MultiCard项
+// 仅针对ActionCard类型
+func (r RobotCustom) MultiCard(title, url string) RobotOption {
+	return func(msg *robotMsg) {
+		msg.ActionCard.Btns = append(msg.ActionCard.Btns, robotActionCardBtn{
+			Title:     title,
+			ActionURL: url,
+		})
+	}
+}
+
+// FeedCard 添加一个FeedCard项
+// 仅针对FeedCard类型
+func (r RobotCustom) FeedCard(title, msgURL, picURL string) RobotOption {
+	return func(msg *robotMsg) {
+		msg.FeedCard.Links = append(msg.FeedCard.Links, robotFeedCardLink{
+			Title:      title,
+			MessageURL: msgURL,
+			PicURL:     picURL,
+		})
+	}
 }
